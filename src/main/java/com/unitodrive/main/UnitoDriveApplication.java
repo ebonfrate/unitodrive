7371d02bbef9de48aa2912d93e3771f8d19caee0
dkvit.it@gmail.com <dkvit.it@gmail.com>
2023-03-01 17:56:10 +0100

    build ok

---
+++ src/main/java/com/unitodrive/main/UnitoDriveApplication.java
@@ -0,0 +1,9 @@
+package com.unitodrive.main;
+
+import org.springframework.boot.SpringApplication;
+
+public class UnitoDriveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UnitoDriveApplication.class, args);
+    }
+}
