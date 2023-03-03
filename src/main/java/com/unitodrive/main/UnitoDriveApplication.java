c20f7a36887166c94ac4f059bcb2ccb0f66a88c1
dkvit.it@gmail.com <dkvit.it@gmail.com>
2023-03-03 10:10:40 +0100

    checking docker image build

--- src/main/java/com/unitodrive/main/UnitoDriveApplication.java
+++ src/main/java/com/unitodrive/main/UnitoDriveApplication.java
@@ -1,8 +1,14 @@
 package com.unitodrive.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class UnitoDriveApplication {
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return "Welcome to UnitoDrive!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(UnitoDriveApplication.class, args);
     }
